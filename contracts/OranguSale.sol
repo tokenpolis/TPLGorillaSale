feaba106c211d9b67989b82a0a24f4ef5bce0fbd
dadabit <dcarboni@gmail.com>
2017-12-28 18:10:36 +0100

    better doc

--- contracts/OranguSale.sol
+++ contracts/OranguSale.sol
@@ -8,18 +8,25 @@ import "./OranguToken.sol";
 
 /**
  * @title OranguSale
- * @dev This is an example of a fully fledged crowdsale.
+ * @dev This is an example of crowdsale based on Zeppelin
+ * Crowdsale and CappedCrowdsale and Pausable. See relevant docs
  * The way to add new features to a base crowdsale is by multiple inheritance.
  * In this example we are providing following extensions:
- * CappedCrowdsale - sets a max boundary for raised funds
- *
- * After adding multiple features it's good practice to run integration tests
- * to ensure that subcontracts works together as intended.
+ * - set rate during crowdsale
+ * - pause / resume crowdsale
+ * - premint a number _premined of token at creation and assigns to _preminedOwner
+ * - allow owner to take ownership of Token contract
+ * 
  */
 contract OranguSale is CappedCrowdsale,Ownable, Pausable {
 
+
+  //the boundaries within rate can be set
   uint256  public MAXRATE;
   uint256  public MINRATE;
+
+
+
   function OranguSale(     uint256 _time_start,
                            uint256 _time_end,
                            uint256 _rate,
@@ -40,10 +47,13 @@ contract OranguSale is CappedCrowdsale,Ownable, Pausable {
       token.mint(_preminedOwner,_premined);
   }
 
+
+  //override Crowdsale.createTokenContract()
   function createTokenContract() internal returns (MintableToken) {
     return new OranguToken();
   }
 
+  // allows owner to set rate within boundaries
   function setRate(uint256 _rate) onlyOwner public{
     require( _rate >= MINRATE);
     require( _rate <= MAXRATE);
@@ -55,12 +65,13 @@ contract OranguSale is CappedCrowdsale,Ownable, Pausable {
     super.buyTokens(beneficiary);
   }
 
+  //adding pause to fallback
   function   () payable external whenNotPaused  {
     buyTokens(msg.sender);
   }
 
-  //this function transfers the token contract ownership to sale owner
-  //this is dangerous, use only if a disaster happens with this sale
+  // this function transfers the token contract ownership to sale owner
+  // this is dangerous, use only if a disaster happens with this sale
   // and you want to take the contract safe out of it
   function takeTokenContractOwnership() onlyOwner public{
     token.transferOwnership(owner);
--- contracts/OranguToken.sol
+++ contracts/OranguToken.sol
@@ -5,11 +5,11 @@ import "zeppelin-solidity/contracts/token/CappedToken.sol";
 /**
  * @title OranguToken
  * @dev Very simple ERC20 Token, capped and minted.
- * It is meant to be used in a crowdsale contract.
+ * It is meant to be used in a OranguSale crowdsale contract.
  */
 contract OranguToken is CappedToken {
 
-  string  public constant name            = "Crip.token(test)";
+  string  public constant name            = "Crip.token(test)"; //remove "(test)" in production
   string  public constant symbol          = "CRI";
   uint8   public constant decimals        = 18;
   uint256 public constant max_supply      = 50000000 * 10**18;//50ml and 18 decimals
