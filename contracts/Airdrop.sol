c2d797489ef0db4d2e7f06e32413bb4957e4c98c
dadabit <dcarboni@gmail.com>
2018-02-18 19:20:52 +0100

    added testcase to airdrop and gas usage

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -1,37 +1,31 @@
 pragma solidity ^0.4.17;
 
 
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
- */
-contract ERC20Basic {
-  function totalSupply() public view returns (uint256);
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
-  event Transfer(address indexed from, address indexed to, uint256 value);
-}
+import "zeppelin-solidity/contracts/token/ERC20Basic.sol";
 
 
 contract Airdrop{
 	address[] public recipients;
 	uint256[] public balances;
 	ERC20Basic public token;
+  address owner;
 
 
-	function Airdrop(ERC20Basic _token) public{
+	function Airdrop(address _token) public{
 		require(_token != address(0));
-		token = _token;
+		token = ERC20Basic(_token);
+    owner = msg.sender;
 	}
 
 	function setRecipientsAndBalances(address[] _recipients, uint256[] _balances) public {
 		require(_recipients.length == _balances.length);
+    require(msg.sender == owner);
 		recipients = _recipients;
 		balances = _balances;
 	}
 
     function doAirdrop() public returns(uint){
+      require(msg.sender == owner);
     	require(recipients.length>0);
     	for(uint i=0; i < recipients.length; i++){
     		if(token.balanceOf(this)>=balances[i]){
--- test/airdrop.js
+++ test/airdrop.js
@@ -13,12 +13,14 @@ const ether      = 1e18 * wei;
 
 const OranguToken = artifacts.require('./OranguToken.sol');
 const Airdrop     = artifacts.require('./Airdrop.sol');
-contract('Airdrop', function ([owner, wallet, investor]) {
+contract('Airdrop', function ([owner, other]) {
 
     beforeEach(async function () {
     this.token      = await OranguToken.new();
-    this.airdrop    = await Airdrop.new(this.token);
-    await this.token.mint(this.airdrop, 3.14 * 10**18);
+    console.log("token at:"+this.token.address);
+    this.airdrop    = await Airdrop.new(this.token.address);
+    console.log("airdrop at:"+this.airdrop.address);
+    await this.token.mint(this.airdrop.address, 3.14 * 10**18);
   });
 
 
@@ -26,8 +28,70 @@ contract('Airdrop', function ([owner, wallet, investor]) {
     
     this.token.should.exist;
     this.airdrop.should.exist;
-    (await this.token.balanceOf(this.airdrop)).should.be.bignumber.equal(3.14 * 10**18);
+    (await this.token.balanceOf(this.airdrop.address)).should.be.bignumber.equal(3.14 * 10**18);
 
   });
+
+  it('should set recipients and balances', async function () {
+    
+    let addr     = ["0x0000000000000000000000000000000000000001",
+                    "0x0000000000000000000000000000000000000002",
+                    "0x0000000000000000000000000000000000000003"];
+
+    let balances = [1000,2000,3000];
+
+    await this.airdrop.setRecipientsAndBalances(addr,balances, {from:other}).should.be.rejected;
+    await this.airdrop.setRecipientsAndBalances(addr,balances).should.be.fulfilled;
+    
+    (await this.airdrop.recipients(0)).should.be.equal(addr[0]);
+    (await this.airdrop.balances(0)).should.be.bignumber.equal(balances[0]);
+
+    (await this.airdrop.recipients(1)).should.be.equal(addr[1]);
+    (await this.airdrop.balances(1)).should.be.bignumber.equal(balances[1]);
+
+    (await this.airdrop.recipients(2)).should.be.equal(addr[2]);
+    (await this.airdrop.balances(2)).should.be.bignumber.equal(balances[2]);
+
+  });
+
+  it('should do the airdrop', async function () {
+    
+    let addr     = ["0x0000000000000000000000000000000000000001",
+                    "0x0000000000000000000000000000000000000002",
+                    "0x0000000000000000000000000000000000000003"];
+    let balances = [1000,2000,3000];
+
+    await this.airdrop.setRecipientsAndBalances(addr,balances).should.be.fulfilled;
+
+    await this.airdrop.doAirdrop({from:other}).should.be.rejected;
+    await this.airdrop.doAirdrop().should.be.fulfilled;
+    (await this.token.balanceOf(addr[0])).should.be.bignumber.equal(balances[0]);
+    (await this.token.balanceOf(addr[1])).should.be.bignumber.equal(balances[1]);
+    (await this.token.balanceOf(addr[2])).should.be.bignumber.equal(balances[2]);
+  });
+
+
+  it('should do massive airdrop to cause gas issues', async function () {
+    
+    var a = [];
+
+    for (i=100;i<200;i++) a.push(i);
+    // pass a function to map
+    const addr = a.map(x => "0x0000000000000000000000000000000000000" + x );
+
+    console.log(addr);
+
+    let balances = addr.map(x => 1000);
+    console.log(balances);    
+
+
+    await this.airdrop.setRecipientsAndBalances(addr,balances).should.be.fulfilled;
+
+    await this.airdrop.doAirdrop({from:other}).should.be.rejected;
+    let result = await this.airdrop.doAirdrop();
+    console.log("gasUsed:"+result.receipt.gasUsed);
+
+  });
+
 
 });
